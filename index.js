38da084d658f59e66258ba271f245d5fceb823fc
Maxi Jofré <maxijofre.c@gmail.com>
2021-07-15 11:08:37 -0300

    Modificando li terminado

--- index.js
+++ index.js
@@ -25,6 +25,30 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function removiendoLi() {
+  const listasEl = document.querySelectorAll("ul li");
+
+  listasEl.forEach(element => {
+    element.remove()
+  });
+}
+
+function creandoLiNuevos() {
+  const ul = document.querySelector("ul");
+
+  return cosasQueAprendimos.forEach(element => {
+    const nuevasListas = document.createElement("li");
+    ul.appendChild(nuevasListas);
+    
+    nuevasListas.textContent = element.tema;
+    nuevasListas.classList = element.class;
+  })
+  
+}
+
+function main() {
+  removiendoLi();
+  creandoLiNuevos();
+}
 
 main();
